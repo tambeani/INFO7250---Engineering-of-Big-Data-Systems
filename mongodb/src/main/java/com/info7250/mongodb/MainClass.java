c3daa36a33653ddac6922da3d3fcde6f97043746
Andy <tambe.ani@northeastern.edu>
2022-05-30 23:04:44 -0400

    connection to mongodb established

--- mongodb/src/main/java/com/info7250/mongodb/MainClass.java
+++ mongodb/src/main/java/com/info7250/mongodb/MainClass.java
@@ -1,10 +1,32 @@
 package com.info7250.mongodb;
 
+import org.bson.Document;
+
+//import com.mongodb.MongoClient;
+
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Step 1 - Connect to database
+		
+		// Legacy driver
+		com.mongodb.MongoClient legacyClient = new com.mongodb.MongoClient();
+		
+		// Modern driver
+		com.mongodb.client.MongoClient modernClient = com.mongodb.client.MongoClients.create();
+		
+		// Step 2 - Select the DB
+		com.mongodb.client.MongoDatabase db = modernClient.getDatabase("nyse_lab");
+		
+		// Step 3 - Select the collection
+		com.mongodb.client.MongoCollection<Document> coll = db.getCollection("nsye_A");
+		
+		// Step 4 - Create a document
+		Document doc = new Document();
+		doc.append("fname", "Aniruddha");
+		
+		// Step 5 - Insert document into collection
+		coll.insertOne(doc);
 		
 		
 	}
