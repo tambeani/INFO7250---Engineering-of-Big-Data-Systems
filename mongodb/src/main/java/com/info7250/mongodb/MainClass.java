dbc97e994987e21f29e43aaa1b7b0533becdfc37
Andy <tambe.ani@northeastern.edu>
2022-06-05 19:12:45 -0400

    Lab 2

--- mongodb/src/main/java/com/info7250/mongodb/MainClass.java
+++ mongodb/src/main/java/com/info7250/mongodb/MainClass.java
@@ -1,5 +1,6 @@
 package com.info7250.mongodb;
 
+import org.bson.BSON;
 import org.bson.Document;
 
 //import com.mongodb.MongoClient;
@@ -15,20 +16,63 @@ public class MainClass {
 		// Modern driver
 		com.mongodb.client.MongoClient modernClient = com.mongodb.client.MongoClients.create();
 		
+		/* *************************************************************************************
+		 * Connection URI 
+		 * 
+		 * The connection URI specifies the set of instructions for the driver to connect to the 
+		 * MongoDB deployment. Following structure is applied to a valid URI
+		 * 
+		 * mongodb://user:pass@sample.host:27017/<connection options>
+		 * *************************************************************************************
+		 */
+		
 		// Step 2 - Select the DB
 		com.mongodb.client.MongoDatabase db = modernClient.getDatabase("nyse_lab");
 		
 		// Step 3 - Select the collection
 		com.mongodb.client.MongoCollection<Document> coll = db.getCollection("nsye_A");
 		
+		/* *************************************************************************************
+		 * 
+		 * BSON - Binary JSON
+		 * 
+		 * This data format includes all JSON data structure types including date,different sized
+		 * integers, objectids, binary data.
+		 * 
+		 * Its not human-readable, thus must be converted to JSON. The mongodb driver allows us to
+		 * work using the BSON interface by using anyone of the following implementations,
+		 * 
+		 * 1. Document
+		 * 2. BsonDocument
+		 * 
+		 * and many more.
+		 * *************************************************************************************
+		 */
+		
 		// Step 4 - Create a document
 		Document doc = new Document();
 		doc.append("fname", "Aniruddha");
 		
+		/* *************************************************************************************
+		 * 
+		 * 	Document
+		 * 
+		 *  Its an implementation of the interface BSON.
+		 *  
+		 *  Constructors
+		 *  1. Document() - Creates an empty document instance
+		 *  2. Document(String key,String value) - Creates a document with given key/pair
+		 *  3. Document(Map<String,Object> map) - Creates a document with given map
+		 * 
+		 * *************************************************************************************
+		 */
 		// Step 5 - Insert document into collection
-		coll.insertOne(doc);
+		//coll.insertOne(doc);
 		
-		
+		// Step 6 - Retrieve the document from the collection
+		Document ret = coll.find().first();
+		System.out.print(ret.toJson());
+				
 	}
 
 }
