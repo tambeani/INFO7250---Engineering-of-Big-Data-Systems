015826cf459de5bea550d68c77f4c0071ca27239
Andy <tambe.ani@northeastern.edu>
2022-07-23 17:43:01 -0400

    Part 4 - assignment[A

--- mongodb/src/main/java/com/info7250/hadoop/customInputFormat/HitCounter.java
+++ mongodb/src/main/java/com/info7250/hadoop/customInputFormat/HitCounter.java
@@ -1,6 +1,7 @@
 package com.info7250.hadoop.customInputFormat;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.IntWritable;
@@ -10,15 +11,17 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 //public class HitCounter extends Mapper<Text,Text,Text,IntWritable>{ //- KeyValueTextInputFormat 
 //public class HitCounter extends Mapper<LongWritable,Text,Text,IntWritable>{ //- NLineInputFormat
-public class HitCounter extends Mapper<BytesWritable,Text,Text,IntWritable>{ //- FixedLengthFormat
+public class HitCounter extends Mapper<LongWritable,BytesWritable,Text,IntWritable>{ //- FixedLength
 		
 	private final static IntWritable one = new IntWritable(1);
 	private Text ipaddress = new Text();
 	
 	//public void map(Text key, Text value, org.apache.hadoop.mapreduce.Mapper.Context context) throws IOException, InterruptedException { //- KeyValueTextInputFormat
-	public void map(BytesWritable key, Text value, org.apache.hadoop.mapreduce.Mapper.Context context) throws IOException, InterruptedException { //- NLineInputFormat
+	//public void map(BytesWritable key, Text value, org.apache.hadoop.mapreduce.Mapper.Context context) throws IOException, InterruptedException { //- NLineInputFormat
+	public void map(LongWritable key, BytesWritable value, org.apache.hadoop.mapreduce.Mapper.Context context) throws IOException, InterruptedException { //- FixedInputFormat
 			
-			String line=value.toString();
+			//String line=value.toString();
+			String line = new String(value.get(),StandardCharsets.UTF_8);
 			
 			String[] tokens=line.split(" ");
 			ipaddress.set(tokens[0]);
